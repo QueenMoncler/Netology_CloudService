27da6e48a0eac15863fca7a04ec36da9ab8f8885
queen <Queenmoncler@gmail.com>
2024-01-21 15:07:55 +0300

    initial and change

---
+++ src/main/java/com/example/netology_cloudservice/model/StorageModel.java
@@ -0,0 +1,35 @@
+package com.example.netology_cloudservice.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "files", schema = "netology")
+public class StorageModel{
+
+    @Id
+    @Column(nullable = false, unique = true)
+    private String filename;
+
+    @Column(nullable = true)
+    private LocalDateTime dateTime;
+
+    @Column(nullable = false)
+    private Long size;
+
+    @Lob
+    @Column(nullable = false)
+    private byte[] fileContent;
+
+    @ManyToOne
+    private User user;
+
+}
---
+++ src/main/java/com/example/netology_cloudservice/model/User.java
@@ -0,0 +1,57 @@
+package com.example.netology_cloudservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import jakarta.persistence.*;
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users", schema = "netology")
+public class User implements UserDetails, Serializable {
+
+    @Id
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<StorageModel> userFiles;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
